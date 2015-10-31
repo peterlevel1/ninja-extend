1474375733700f8b2f54f99848e6c44a230ea0d2
Peter <Peter@users.noreply.github.com>
2015-10-31 15:32:53 +0800

    'heihei'

--- index.js
+++ index.js
@@ -37,40 +37,39 @@
 
 		targetIsArray = Array.isArray(target);
 
-		return arguments.length === 0
-			? target
-			: pSlice.call(arguments, index).reduce(function (target, obj) {
-					if (obj == null || !isObject(obj) || !(isPlainObject(obj) || Array.isArray(obj))) {
+		return arguments.length === 0 ? target :
+			pSlice.call(arguments, index).reduce(function (target, obj) {
+				if (obj == null || !isObject(obj) || !(isPlainObject(obj) || Array.isArray(obj))) {
+					return target;
+				}
+
+				Object.keys(obj).reduce(function (target, key) {
+					var value = obj[key];
+					if (value === void 0 || value === target) {
 						return target;
 					}
 
-					Object.keys(obj).reduce(function (target, key) {
-						var value = obj[key];
-						if (value === void 0 || value === target) {
-							return target;
-						}
-
-						if (!deep || !isObject(value)) {
-							if (targetIsArray) {
-								target.push(value);
-							} else {
-								target[key] = value;
-							}
+					if (!deep || !isObject(value)) {
+						if (targetIsArray) {
+							target.push(value);
 						} else {
-							var	data =
-								Array.isArray(value) ?
-									Array.isArray(target[key]) ? target[key] : [] :
-								isPlainObject(value) ?
-									isPlainObject(target[key]) ? target[key] : {} :
-								{};
-							target[key] = extend(true, data, value);
+							target[key] = value;
 						}
-
-						return target;
-					}, target);
+					} else {
+						var	data =
+							Array.isArray(value) ?
+								Array.isArray(target[key]) ? target[key] : [] :
+							isPlainObject(value) ?
+								isPlainObject(target[key]) ? target[key] : {} :
+							{};
+						target[key] = extend(true, data, value);
+					}
 
 					return target;
 				}, target);
+
+				return target;
+			}, target);
 	}
 
 })(this);
