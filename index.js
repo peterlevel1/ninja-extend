19a0de558b8c61ca2281068b5657031762d6012e
Peter <Peter@users.noreply.github.com>
2015-10-31 14:59:42 +0800

    'heihei'

---
+++ index.js
@@ -0,0 +1,74 @@
+;(function (global) {
+	(typeof define !== 'undefined' && define.amd && define(function () { return extend; })) ||
+	(typeof module !== 'undefined' && (module.exports = extend)) ||
+	(global.extend = extend);
+
+	var toString = Object.prototype.toString;
+	var pSlice = Array.prototype.slice;
+
+	function isObject(obj, isPlain) {
+		return obj == null ? false :
+			typeof obj !== 'object' ? false :
+			!isPlain ? true : toString.call(obj) === '[object Object]' &&
+				obj.constructor === Object;
+	}
+
+	function isPlainObject(obj) {
+		return isObject(obj, true) &&
+			obj.nodeType === void 0 &&
+			obj.window === void 0;
+	}
+
+	function extend() {
+		var target;
+		var deep = arguments[0] === true;
+		var index = 1;
+		var targetIsArray;
+
+		target = typeof arguments[0] === 'boolean'
+			? ++index && arguments[1]
+			: arguments[0];
+
+		target = isObject(target) || typeof target === 'function'
+			? target
+			: {};
+
+		arguments.length <= 1 && !(index = 0) && (target = this);
+
+		targetIsArray = Array.isArray(target);
+
+		return pSlice.call(arguments, index).reduce(function (target, obj) {
+			if (obj == null || !isObject(obj) || !(isPlainObject(obj) || Array.isArray(obj))) {
+				return target;
+			}
+
+			Object.keys(obj).reduce(function (target, key) {
+				var value = obj[key];
+				if (value === void 0 || value === target) {
+					return target;
+				}
+
+				if (!deep || !isObject(value)) {
+					if (targetIsArray) {
+						target.push(value);
+					} else {
+						target[key] = value;
+					}
+				} else {
+					var	data =
+						Array.isArray(value) ?
+							Array.isArray(target[key]) ? target[key] : [] :
+						isPlainObject(value) ?
+							isPlainObject(target[key]) ? target[key] : {} :
+						{};
+					target[key] = extend(true, data, value);
+				}
+
+				return target;
+			}, target);
+
+			return target;
+		}, target);
+	}
+
+})(this);
---
+++ test-extend.js
@@ -0,0 +1,105 @@
+var extend = typeof module !== 'undefined'
+	? require('./index.js')
+	: this.extend;
+
+//----------------------------------------------
+// shallow extend
+
+//common array extend
+var obj = [1, 2];
+var data = extend([], obj);
+obj[0]++;
+console.log(data[0] !== obj[0]);
+console.log(obj[0] === 2);
+console.log(data[0] === 1);
+
+// common object extend
+var obj = {x : 1, y : 1};
+var data = extend({}, obj);
+obj.x++;
+console.log(data.x !== obj.x);
+console.log(obj.x === 2);
+console.log(data.x === 1);
+
+//----------------------------------------------
+// deep extend
+
+// extend Array
+var obj = [[1]];
+var data = extend(true, [], obj);
+obj[0][0]++;
+console.log(data[0][0] !== obj[0][0]);
+console.log(obj[0][0] === 2);
+console.log(data[0][0] === 1);
+
+// extend Object
+var obj = { x : { y : 1 } };
+var data = extend(true, {}, obj);
+obj.x.y++;
+console.log(data.x.y !== obj.x.y);
+console.log(obj.x.y === 2);
+console.log(data.x.y === 1);
+
+//----------------------------------------------
+// if frontend
+if (this.window) {
+	// if deep extend a dom object
+	var div = document.createElement('div');
+	var data = extend(true, {}, obj);
+	console.log(data.nodeName === void 0);
+
+	var data = extend(true, {}, window);
+	console.log(data.window === void 0);
+} else {
+	// if deep extend a emulated DIV Object
+	var obj = { nodeType: 1, nodeName : 'div' };
+	var data = extend(true, {}, obj);
+	console.log(data.nodeName === void 0);
+
+	// if deep extend a emulated window object
+	var obj = {};
+	obj.window = obj;
+	var data = extend(true, {}, obj);
+	console.log(data.window === void 0);
+}
+
+// to be used as a puppet
+var obj = {
+	extend : extend
+};
+
+obj.extend({
+	say : function () {
+		return 'hello world';
+	}
+});
+console.log(obj.say() === 'hello world');
+
+// deep extend a array, and save the src value
+var data = {
+	desc : ['I am Peter']
+};
+data = extend(true, data, {
+	desc : ['and Who are you ?']
+});
+console.log(data.desc[0] === 'I am Peter');
+console.log(data.desc[1] === 'and Who are you ?');
+
+// deep extend a obj, and save the src value
+var data = {
+	would : {
+		you : 'mind if I know your QQ || email || other contacts'
+	}
+};
+data = extend(true, data, {
+	would : {
+		youMindIf : 'leave me a contact of you, would\'nt you ?'
+	}
+});
+console.log(data.would.you === 'mind if I know your QQ || email || other contacts');
+console.log(data.would.youMindIf === 'leave me a contact of you, would\'nt you ?');
+
+
+console.log('the test is over, check the results');
+console.log('make sure JS is running in ES5 mode');
+console.log('if there is an error, please let me know');
